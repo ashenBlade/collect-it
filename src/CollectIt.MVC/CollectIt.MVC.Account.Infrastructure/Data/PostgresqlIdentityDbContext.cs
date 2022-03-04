36814656da70e4fd83e897fafe2d0d2d94f98e21
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-04 20:54:56 +0300

    Add infrastructure project for account part

---
+++ src/CollectIt.MVC/CollectIt.MVC.Account.Infrastructure/Data/PostgresqlIdentityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CollectIt.MVC.Account.IdentityEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectIt.MVC.Account.Infrastructure.Data;
+
+public class PostgresqlIdentityDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
+    public DbSet<UserSubscription> UsersSubscriptions { get; set; }
+
+    public PostgresqlIdentityDbContext(DbContextOptions<PostgresqlIdentityDbContext> options)
+        : base(options)
+    { }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+               .HasMany(u => u.Subscriptions)
+               .WithMany(s => s.Subscribers)
+               .UsingEntity<UserSubscription>();
+    }
+}
