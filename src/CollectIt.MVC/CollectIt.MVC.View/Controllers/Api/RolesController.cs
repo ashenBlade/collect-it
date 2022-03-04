e9ab0d63568fba2cc909c253132d643237fce93c
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-04 20:55:22 +0300

    Create classes for main controllers for account api

---
+++ src/CollectIt.MVC/CollectIt.MVC.View/Controllers/Api/RolesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectIt.MVC.View.Controllers.Api;
+
+[ApiController]
+[Route("api/v1/roles")]
+public class RolesController : ControllerBase
+{
+    
+}
---
+++ src/CollectIt.MVC/CollectIt.MVC.View/Controllers/Api/SubscriptionsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectIt.MVC.View.Controllers.Api;
+
+[ApiController]
+public class SubscriptionsController : ControllerBase
+{
+    
+}
