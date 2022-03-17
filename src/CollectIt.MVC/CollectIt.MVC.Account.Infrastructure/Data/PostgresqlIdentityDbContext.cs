5a408fec4864fd55d9ad5621da0f6c34050d10be
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-17 20:49:34 +0300

    Remove ActiveUsersSubscriptions table from DbContext

--- src/CollectIt.MVC/CollectIt.MVC.Account.Infrastructure/Data/PostgresqlIdentityDbContext.cs
+++ src/CollectIt.MVC/CollectIt.MVC.Account.Infrastructure/Data/PostgresqlIdentityDbContext.cs
@@ -10,18 +10,16 @@ public class PostgresqlIdentityDbContext : IdentityDbContext<User, Role, int>
     public DbSet<Role> Roles { get; set; }
     public DbSet<Subscription> Subscriptions { get; set; }
     public DbSet<UserSubscription> UsersSubscriptions { get; set; }
-    public DbSet<ActiveUserSubscription> ActiveUserSubscriptions { get; set; }
     public PostgresqlIdentityDbContext(DbContextOptions<PostgresqlIdentityDbContext> options)
         : base(options)
     { }
     
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
         builder.Entity<User>()
                .HasMany(u => u.Subscriptions)
                .WithMany(s => s.Subscribers)
                .UsingEntity<UserSubscription>();
-        builder.Entity<ActiveUserSubscription>()
-               .ToView("active_users_subscriptions");
     }
 }
