5cde77a20e85af30ac97a4aa0f2f7c86e1d417c0
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-18 08:54:26 +0300

    Update constraint for UserSubscription
    Add condition for subscriptions with LeftResourcesCount > 0

--- src/CollectIt.MVC/CollectIt.MVC.View/Migrations/Accounts/20220317183949_AddIdentityDbContext.cs
+++ src/CollectIt.MVC/CollectIt.MVC.View/Migrations/Accounts/20220317183949_AddIdentityDbContext.cs
@@ -216,7 +216,8 @@ namespace CollectIt.MVC.View.Migrations.Accounts
                 builder.Sql(@"
         ALTER TABLE ""UsersSubscriptions"" 
         ADD CONSTRAINT ""MAX_1_SUBSCRIPTION_OF_SAME_TYPE_PER_USER_AT_TIME"" 
-        EXCLUDE USING gist(""Id"" WITH =, ""SubscriptionId"" WITH =, ""During"" WITH &&);
+        EXCLUDE USING gist(""Id"" WITH =, ""SubscriptionId"" WITH =, ""During"" WITH &&)
+        WHERE (""LeftResourcesCount"" > 0);
                 ");
             }
             else
