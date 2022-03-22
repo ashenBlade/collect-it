8188c85ad6089739ae759903ea4245dcceb481b1
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-03-22 21:01:33 +0300

    [add]repository abstractions

---
+++ src/CollectIt.MVC/CollectIt.MVC.Abstractions/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mime;
+using CollectIt.MVC.Account.Abstractions.Interfaces;
+using CollectIt.MVC.Resources.Entities;
+
+namespace CollectIt.MVC.Resources.Abstractions;
+
+public interface IImageRepository : IDerivedResourceRepository<Image, int>
+{
+    
+}
