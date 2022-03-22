3f3ebc931d2fd8555e390800f62fa150220eb8d9
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-03-22 21:01:58 +0300

    [add]based repository realisation

---
+++ src/CollectIt.MVC/CollectIt.MVC.Infrastructure/Repositories/ResourceRepository.cs
@@ -0,0 +1,39 @@
+﻿using CollectIt.MVC.Account.Abstractions.Interfaces;
+using CollectIt.MVC.Resources.Abstractions;
+using CollectIt.MVC.Resources.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectIt.MVC.Resources.Infrastructure.Repositories;
+
+public class ResourceRepository : IResourceRepository
+{
+    private readonly PostgresqlResourcesDbContext context;
+
+    public ResourceRepository(PostgresqlResourcesDbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<int> AddAsync(Resource item)
+    {
+        await context.Resources.AddAsync(item);
+        await context.SaveChangesAsync();
+        return item.ResourceId;
+    }
+
+    public async Task<Resource> FindByIdAsync(int id)
+    {
+        return await context.Resources.Where(resource => resource.ResourceId == id).FirstOrDefaultAsync();
+    }
+
+    public Task UpdateAsync(Resource item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RemoveAsync(Resource item)
+    {
+        context.Resources.Remove(item);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ src/CollectIt.MVC/CollectIt.MVC.Infrastructure/Repositories/VideoRepository.cs
@@ -0,0 +1,42 @@
+﻿using CollectIt.MVC.Resources.Abstractions;
+using CollectIt.MVC.Resources.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectIt.MVC.Resources.Infrastructure.Repositories;
+
+public class VideoRepository : IVideoRepository
+{
+    private readonly PostgresqlResourcesDbContext context;
+    private readonly ResourceRepository resourceRepository;
+
+    public VideoRepository(PostgresqlResourcesDbContext context)
+    {
+        this.context = context;
+        resourceRepository = new ResourceRepository(context);
+    }
+
+    public async Task<int> AddAsync(Video item, Resource resource)
+    {
+        await context.Videos.AddAsync(item);
+        await context.SaveChangesAsync();
+        await resourceRepository.AddAsync(resource);
+        return item.VideoId;
+    }
+
+    public async Task<Video> FindByIdAsync(int id)
+    {
+        return await context.Videos.Where(video => video.VideoId == id).FirstOrDefaultAsync();
+    }
+
+    public Task UpdateAsync(Video item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RemoveAsync(Video item, Resource resource)
+    {
+        context.Videos.Remove(item);
+        await context.SaveChangesAsync();
+        await resourceRepository.RemoveAsync(resource);
+    }
+}
