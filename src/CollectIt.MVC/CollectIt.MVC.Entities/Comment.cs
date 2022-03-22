5624c4718be68bb2791498c8ee9f0d2804333d56
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-03-22 21:00:43 +0300

    [add]Resources and comments database tables

---
+++ src/CollectIt.MVC/CollectIt.MVC.Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CollectIt.MVC.Account.IdentityEntities;
+
+namespace CollectIt.MVC.Resources.Entities;
+
+public class Comment
+{
+    [Key]
+    public int CommentId { get; set; }
+    
+    [Required]
+    public User Owner { get; set; }
+    
+    [Required]
+    public string Content { get; set; }
+    
+    [Required]
+    public DateTime UploadDate { get; set; }
+    
+    [Required]
+    public Resource Target { get; set; }
+}
