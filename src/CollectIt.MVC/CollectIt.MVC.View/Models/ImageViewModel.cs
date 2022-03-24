cfa86ebdeca42751db6f2d1936527dc677532869
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-03-24 15:49:40 +0300

    [add] image view model

---
+++ src/CollectIt.MVC/CollectIt.MVC.View/Models/ImageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CollectIt.Database.Entities.Account;
+
+namespace CollectIt.MVC.View.Models;
+
+public class ImageViewModel
+{
+    [Required]
+    public User Owner { get; set; }
+
+    public DateTime UploadDate { get; set; }
+
+    public string Path { get; set; }
+}
