556e641b55661b9512fd5617074c52d010c59eef
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-03-24 15:49:54 +0300

    [add] controller for resources

---
+++ src/CollectIt.MVC/CollectIt.MVC.View/Controllers/ResourceController.cs
@@ -0,0 +1,33 @@
+﻿using CollectIt.Database.Infrastructure;
+using CollectIt.Database.Infrastructure.Resources.Repositories;
+using CollectIt.MVC.View.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectIt.MVC.View.Controllers;
+
+[Route("resource")]
+public class ResourceController : Controller
+{
+    private ImageRepository imageRepository;
+    private ResourceRepository resourceRepository;
+
+    public ResourceController(PostgresqlCollectItDbContext context)
+    {
+        imageRepository = new ImageRepository(context);
+        resourceRepository = new ResourceRepository(context);
+    }
+
+    [HttpGet]
+    [Route("image")]
+    public async Task<IActionResult> Image(int id)
+    {
+        var source = imageRepository.FindByIdAsync(id).Result;
+        var imgModel = new ImageViewModel()
+        {
+            Owner = source.Resource.ResourceOwner,
+            UploadDate = source.Resource.UploadDate,
+            Path = source.Resource.ResourcePath
+        };
+        return View(imgModel);
+    }
+}
