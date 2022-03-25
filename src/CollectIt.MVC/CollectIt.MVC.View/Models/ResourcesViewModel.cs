3303f5ff1938b92a7a1e176894b73728b3679914
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2022-03-25 17:34:16 +0300

    update
    add view components

---
+++ src/CollectIt.MVC/CollectIt.MVC.View/Models/ResourcesViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CollectIt.Database.Entities.Resources;
+
+namespace CollectIt.MVC.View.Models;
+
+public class ResourcesViewModel
+{
+    public IReadOnlyList<Resource> Resources { get; set; }
+}
