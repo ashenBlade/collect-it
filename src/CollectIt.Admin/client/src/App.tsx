7fb24df66652f571752e9a51883a344dbc2e2d01
ashblade <htcdnsnnkom2002@gmail.com>
2022-03-28 21:20:24 +0300

    Update client and server projects setup
    Add basic logic, add packages

--- src/CollectIt.Admin/client/src/App.tsx
+++ src/CollectIt.Admin/client/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is template for <a href='https://github.com/ashenBlade/collect-it'>Admin page</a> project
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
