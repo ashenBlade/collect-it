59e16260a704261ee997077bd7d46246543f7acc
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-04-13 17:14:12 +0300

    find image by id method and test

---
+++ src/CollectIt.API/CollectIt.API.WebAPI/Controllers/Resources/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using CollectIt.API.DTO.Mappers;
+using CollectIt.Database.Abstractions.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectIt.API.WebAPI.Controllers.Resources;
+
+[ApiController]
+[Route("api/images")]
+public class ImageController : Controller
+{
+    private IImageManager _imageManager;
+    
+    public ImageController(IImageManager imageManager)
+    {
+        _imageManager = imageManager;
+    }
+
+    [HttpGet("{id:int}")]
+    public  async Task<IActionResult> FindImageById(int id)
+    {
+        var image = await _imageManager.FindByIdAsync(id);
+        if (image is null)
+            return NotFound();
+        await using (var fileStream = new FileStream(image.Address, FileMode.Open))
+        {
+            var buffer = new byte[fileStream.Length];
+            await fileStream.ReadAsync(buffer);
+            return Ok(ResourcesMappers.ToReadImageDTO(image, buffer));
+        }
+    }
+}
