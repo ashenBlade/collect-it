bbe791cfb00eaf992f91859aac65daa505a41d5d
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-17 21:42:06 +0300

    Add Sequelize and Config to dependencies

--- src/CollectIt.Admin/server/src/app.module.ts
+++ src/CollectIt.Admin/server/src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize'
+import { UsersModule } from './users/users.module';
+import {UsersService} from "./users/users.service";
+import {UsersController} from "./users/users.controller";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [
+      ConfigModule.forRoot({
+          envFilePath: '.env'
+      }),
+      SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'password',
+          database: 'library',
+          autoLoadModels: true,
+          synchronize: true,
+      }),
+      UsersModule
+  ],
 })
 export class AppModule {
 
