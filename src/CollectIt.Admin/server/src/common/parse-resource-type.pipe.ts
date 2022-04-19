4a2ffeabdf232b30a6ec0c458404887deaa5614e
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-19 13:03:44 +0300

    Add basic controller methods for subscriptions

---
+++ src/CollectIt.Admin/server/src/common/parse-resource-type.pipe.ts
@@ -0,0 +1,39 @@
+import {ArgumentMetadata, Injectable, PipeTransform} from "@nestjs/common";
+import {ValidationError} from "sequelize";
+import {ResourceType} from "./resource-type";
+
+@Injectable()
+export class ParseResourceTypePipe implements PipeTransform {
+    transform(value: any, metadata: ArgumentMetadata): ResourceType {
+        if (value instanceof String) {
+            switch (value.toLowerCase()) {
+                case 'any':
+                    return ResourceType.Any;
+                case 'image':
+                    return ResourceType.Image;
+                case 'video':
+                    return ResourceType.Video;
+                case 'music':
+                    return ResourceType.Music;
+                default:
+                    throw new ValidationError(`Could not parse resource type: ${value}`, [])
+            }
+        } else if (value instanceof Number) {
+            switch (value) {
+                case 0:
+                    return ResourceType.Any;
+                case 1:
+                    return ResourceType.Image;
+                case 2:
+                    return ResourceType.Video;
+                case 3:
+                    return ResourceType.Music;
+                default:
+                    throw new ValidationError(`Could not parse resource type: ${value}`, [])
+            }
+
+        }
+        throw new ValidationError('Resource type is not in valid form', [])
+    }
+
+}
