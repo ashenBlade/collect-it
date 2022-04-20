b369b8dd9b85ce29e8177f702ab6be814ff7d7bd
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-20 09:58:13 +0300

    Add redirection to lists and edit pages

--- src/CollectIt.Admin/client/src/components/MainApp.tsx
+++ src/CollectIt.Admin/client/src/components/MainApp.tsx
@@ -4,6 +4,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UsersList from "./pages/users/UsersList";
 import SubscriptionsList from "./pages/subscriptions/SubscriptionsList";
 import ResourcesList from "./pages/resources/ResourcesList";
+import EditSubscription from "./pages/subscriptions/EditSubscription";
+import EditUser from "./pages/users/EditUser";
 
 const MainApp = () => {
     return (
@@ -11,9 +13,20 @@ const MainApp = () => {
             <BrowserRouter>
                 <NavigationPanel />
                 <Routes>
-                    <Route path='/resources' element={<ResourcesList/>}/>
-                    <Route path='/subscriptions' element={<SubscriptionsList/>} />
-                    <Route path='/users' element={<UsersList/>}/>
+                    <Route path='/resources'>
+                        <Route path='' element={<ResourcesList/>}/>
+                        <Route path=':resourceId' element={null}/>
+                    </Route>
+
+                    <Route path='/subscriptions'>
+                        <Route path='' element={<SubscriptionsList/>}/>
+                        <Route path=':subscriptionId' element={<EditSubscription/>}/>
+                    </Route>
+                    <Route path='/users'>
+                        <Route path='' element={<UsersList/>}/>
+                        <Route path=':userId' element={<EditUser/>}/>
+                    </Route>
+                    { /* Fallback */ }
                     <Route path='*' element={<UsersList/>}/>
                 </Routes>
             </BrowserRouter>
---
+++ src/CollectIt.Admin/client/src/components/pages/subscriptions/EditSubscription.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditSubscription = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default EditSubscription;
