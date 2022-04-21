baa2cd7bd88e80f0d2b916f5cb4cbaa6d54a1b4d
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-21 21:39:57 +0300

    [feat] add main entities for admin project

---
+++ src/CollectIt.Admin/client/src/components/entities/subscription.ts
@@ -0,0 +1,13 @@
+import {ResourceType} from "./resource-type";
+import Restriction from "./restriction";
+
+export default interface Subscription {
+    readonly id: number;
+    readonly name: string;
+    readonly description: string;
+    readonly monthDuration: number;
+    readonly appliedResourceType: ResourceType;
+    readonly price: number;
+    readonly restriction: Restriction | null;
+    readonly active: boolean;
+}
---
+++ src/CollectIt.Admin/client/src/components/entities/user.ts
@@ -0,0 +1,11 @@
+import Subscription from "./subscription";
+import Resource from "./resource";
+
+export default interface User {
+    readonly id: number;
+    readonly username: string;
+    readonly email: string;
+    readonly roles: string[];
+    readonly subscriptions: Subscription[];
+    readonly authorOf: Resource[];
+}
