96c265d133c29083cd2d9b347f1805d9c6f80fd7
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-23 12:27:20 +0300

    [feat] add simple images fetch logic for ResourceService

--- src/CollectIt.Admin/client/src/services/ResourceService.ts
+++ src/CollectIt.Admin/client/src/services/ResourceService.ts
@@ -1,15 +1,30 @@
 import {serverAddress as server} from "../constants";
+import useAuthFetch from "./AuthorizedFetch";
+import {ResourceType} from "../components/entities/resource-type";
+import Image from "../components/entities/image";
 
 export class ResourceService {
-
-    async getImagesPagedAsync({pageSize, pageNumber}: {pageSize: number, pageNumber: number}) {
+    private readonly fetch: (info: RequestInfo,
+                             init: RequestInit | null) => Promise<Response>;
+    constructor() {
+        this.fetch = useAuthFetch();
+    }
+    async getImagesPagedAsync({pageSize, pageNumber}: {pageSize: number, pageNumber: number})
+        : Promise<Image[]> {
         if (pageSize < 1 || pageNumber < 1) {
             throw new Error('Page size and page number must be positive');
         }
-        const images = await fetch(`${server}/api/v1/images`, {
+        const response = await this.fetch(`${server}/api/v1/images`, {
             body: JSON.stringify({
-
-            })
-        })
+                pageNumber: pageNumber,
+                pageSize: pageSize,
+                type: ResourceType.Image
+            }),
+            method: 'GET'
+        });
+        if (!response.ok) {
+            throw new Error('Could not get images from server');
+        }
+        return await response.json();
     }
 }
