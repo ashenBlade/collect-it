39fae2d1bb18315c4677cfc5f4cae1a9c92772d2
ashblade <htcdnsnnkom2002@gmail.com>
2022-04-28 21:00:57 +0300

    fix: redirect url after image purchase

--- src/CollectIt.MVC/CollectIt.MVC.View/Controllers/ResourceAcquiringController.cs
+++ src/CollectIt.MVC/CollectIt.MVC.View/Controllers/ResourceAcquiringController.cs
@@ -34,7 +34,7 @@ public class ResourceAcquiringController : Controller
         {
             var acquired = await _resourceAcquisitionService.AcquireImageAsync(userId, imageId);
             _logger.LogInformation("User (Id = {UserId}) successfully acquired image (Id = {ImageId})", userId, imageId);
-            return RedirectToAction("Image", "Images", new {id = imageId});
+            return RedirectToAction("Image", "Images", new {imageId = imageId});
         }
         catch (UserAlreadyAcquiredResourceException alreadyAcquiredResourceException)
         {
@@ -58,7 +58,7 @@ public class ResourceAcquiringController : Controller
         try
         {
             var acquired = await _resourceAcquisitionService.AcquireMusicAsync(userId, musicId);
-            _logger.LogInformation("User (Id = {UserId}) successfully acquired image (Id = {musicId})", userId, musicId);
+            _logger.LogInformation("User (Id = {UserId}) successfully acquired image (Id = {MusicId})", userId, musicId);
             return RedirectToAction("Image", "Images", new {id = musicId});
         }
         catch (UserAlreadyAcquiredResourceException alreadyAcquiredResourceException)
