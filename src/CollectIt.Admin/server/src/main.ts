2a62403278094b3dbf341c59b70c5a7e8053ac51
Iskander <faceitlekro@yandex.ru>
2022-04-30 12:09:22 +0300

    fix errors

--- src/CollectIt.Admin/server/src/main.ts
+++ src/CollectIt.Admin/server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const PORT = 7000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
