d80fad173627b461e345198cbe1de3c69270bbf6
Iskander <faceitlekro@yandex.ru>
2022-04-30 12:08:36 +0300

    fix subs view

--- src/CollectIt.Admin/server/src/main.ts
+++ src/CollectIt.Admin/server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const PORT = 7000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
