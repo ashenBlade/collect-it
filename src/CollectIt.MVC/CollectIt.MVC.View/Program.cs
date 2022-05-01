ed9c283e74fa224bf1b920ca6dd379659983421a
ash.blade <htcdnsnnkom2002@gmail.com>
2022-05-01 21:20:37 +0300

    feat: add google authentication to services

--- src/CollectIt.MVC/CollectIt.MVC.View/Program.cs
+++ src/CollectIt.MVC/CollectIt.MVC.View/Program.cs
@@ -21,15 +21,20 @@ var services = builder.Services;
 
 services.AddControllersWithViews();
 services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = "/account/login";
-    options.DefaultSignOutScheme = "/account/logout";
-}).AddCookie(options =>
-{
-    options.Cookie.Name = "Cookie";
-});
-services.AddAuthorization();
+         {
+             options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+             options.DefaultSignInScheme = "/account/login";
+             options.DefaultSignOutScheme = "/account/logout";
+         })
+        .AddCookie(options =>
+         {
+             options.Cookie.Name = "Cookie";
+         })
+        .AddGoogle(g =>
+         {
+             g.ClientId = builder.Configuration["Google:ClientId"];
+             g.ClientSecret = builder.Configuration["Google:ClientSecret"];
+         });
 services.AddAuthorization();
 services.AddDbContext<PostgresqlCollectItDbContext>(options =>
 {
@@ -59,10 +64,7 @@ Directory.CreateDirectory(musicPath);
 services.AddSignalR();
 services.AddIdentity<User, Role>(config =>
          {
-             config.User = new UserOptions
-                           {
-                               RequireUniqueEmail = true,
-                           };
+             config.User = new UserOptions {RequireUniqueEmail = true,};
              config.Password = new PasswordOptions
                                {
                                    RequireDigit = true,
