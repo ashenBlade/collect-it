a03dc2bddfe1b5ac0d181a64162b8daab14f0105
ash.blade <htcdnsnnkom2002@gmail.com>
2022-05-08 19:04:09 +0300

    add basic getting parsed result function

--- src/CollectIt.API/CollectIt.API.Tests.Integration.FSharp/TestsHelpers.fs
+++ src/CollectIt.API/CollectIt.API.Tests.Integration.FSharp/TestsHelpers.fs
@@ -2,10 +2,13 @@ module CollectIt.API.Tests.Integration.FSharp.TestsHelpers
 
 open System.Collections.Generic
 open System.Net.Http
+open System.Net.Http.Headers
 open System.Net.Http.Json
 open CollectIt.API.Tests.Integration.FSharp.ConnectResult
 open CollectIt.Database.Infrastructure
 open Microsoft.FSharp.Control
+open Newtonsoft.Json
+open Xunit
 open Xunit.Abstractions
 
 let GetBearerForUserAsync
@@ -40,4 +43,42 @@ let GetBearerForUserAsync
             |> Async.AwaitTask
 
         return value.Bearer
+    }
+
+
+let getResultParsedFromJson<'a>
+    (client: HttpClient)
+    (address: string)
+    (bearer: string)
+    (method: HttpMethod option)
+    (outputHelper: ITestOutputHelper option)
+    (content: HttpContent option)
+    =
+    let method =
+        match method with
+        | Some m -> m
+        | None -> HttpMethod.Get
+
+    let content =
+        match content with
+        | Some c -> c
+        | None -> null
+
+    use message = new HttpRequestMessage(method, address, Content = content)
+    message.Headers.Authorization <- AuthenticationHeaderValue("Bearer", bearer)
+
+    async {
+        let! result = client.SendAsync message |> Async.AwaitTask
+
+        let! json =
+            result.Content.ReadAsStringAsync()
+            |> Async.AwaitTask
+
+        match outputHelper with
+        | Some o -> o.WriteLine json
+        | _ -> ()
+
+        let parsed = JsonConvert.DeserializeObject<'a> json
+        Assert.NotNull parsed
+        return parsed
     }
