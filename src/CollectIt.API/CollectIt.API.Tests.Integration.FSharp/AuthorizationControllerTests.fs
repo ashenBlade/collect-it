1c401b37227bed1157ae3eb998ed519ba8bf81b3
ash.blade <htcdnsnnkom2002@gmail.com>
2022-05-09 08:34:25 +0300

    fix: async with fuck

--- src/CollectIt.API/CollectIt.API.Tests.Integration.FSharp/AuthorizationControllerTests.fs
+++ src/CollectIt.API/CollectIt.API.Tests.Integration.FSharp/AuthorizationControllerTests.fs
@@ -1,9 +1,12 @@
 module Tests
 
 open System.Collections.Generic
+open System.Net
 open System.Net.Http
 open System.Net.Http.Json
+open System.Text.Json
 open CollectIt.API.DTO
+open CollectIt.API.DTO.AccountDTO
 open CollectIt.API.Tests.Integration.FSharp.CollectItWebApplicationFactory
 open CollectIt.Database.Infrastructure
 open Xunit
@@ -14,36 +17,80 @@ type AuthorizationControllerTests(factory: CollectItWebApplicationFactory, outpu
     class
         member this._factory = factory
         member this._output = output
+        member private this.log msg = this._output.WriteLine msg
 
         interface IClassFixture<CollectItWebApplicationFactory>
 
         [<Fact>]
         member this.``Should get valid token with admin username and password``() =
-            let admin = PostgresqlCollectItDbContext.AdminUser
-            use client = this._factory.CreateClient()
+            async {
+                let admin =
+                    PostgresqlCollectItDbContext.AdminUser
 
-            let content =
-                new FormUrlEncodedContent(
-                    [ KeyValuePair("grant_type", "password")
-                      KeyValuePair("username", admin.UserName)
-                      KeyValuePair("password", "12345678") ]
-                )
+                use client = this._factory.CreateClient()
 
-            use message =
-                new HttpRequestMessage(HttpMethod.Post, "/connect/token", Content = content)
+                let content =
+                    new FormUrlEncodedContent(
+                        [ KeyValuePair("grant_type", "password")
+                          KeyValuePair("username", admin.UserName)
+                          KeyValuePair("password", "12345678") ]
+                    )
 
-            task {
-                let! response = client.SendAsync message
+                use message =
+                    new HttpRequestMessage(HttpMethod.Post, "/connect/token", Content = content)
+
+                let log x = this.log (x.ToString())
+                log 1
+
+                let! response =
+                    client.SendAsync(message, Async.DefaultCancellationToken)
+                    |> Async.AwaitTask
+
+                log 2
 
                 let! result =
-                    HttpContentJsonExtensions.ReadFromJsonAsync<AccountDTO.OpenIddictResponseSuccess> response.Content
+                    HttpContentJsonExtensions.ReadFromJsonAsync<OpenIddictResponseSuccess>(
+                        response.Content,
+                        JsonSerializerOptions(JsonSerializerDefaults.Web),
+                        Async.DefaultCancellationToken
+                    )
+                    |> Async.AwaitTask
 
+                log 3
                 Assert.NotNull result
                 Assert.Equal("Bearer", result.TokenType)
                 Assert.NotEmpty result.AccessToken
             }
-            |> Async.AwaitTask
-            |> Async.RunSynchronously
+        //            |> Async.AwaitTask
+//            |> Async.RunSynchronously
+
+        [<Fact>]
+        member this.``Register user with valid username, email and password should create new user``() =
+            use client = this._factory.CreateClient()
+            let username = "SomeValidUsername"
+            let password = "SomeP@ssw0rd"
+            let email = "test@mail.ru"
+
+            let form =
+                new FormUrlEncodedContent(
+                    [ KeyValuePair("username", username)
+                      KeyValuePair("password", password)
+                      KeyValuePair("email", email) ]
+                )
+
+            let message =
+                new HttpRequestMessage(HttpMethod.Post, "/connect/register", Content = form)
+
+            task {
+
+                let! response = client.SendAsync message
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode)
+                let! actual = HttpContentJsonExtensions.ReadFromJsonAsync<AccountDTO.ReadUserDTO> response.Content
+                Assert.NotNull actual
+                Assert.Equal(username, actual.UserName)
+                Assert.Equal(email, actual.Email)
+            }
+
 
 
     end
