da92781dcba04b0b43b2dc69ccbae463644da9cb
ash.blade <htcdnsnnkom2002@gmail.com>
2022-05-15 07:52:18 +0300

    fix: error on updating user info

--- src/CollectIt.MVC/CollectIt.MVC.View/ViewModels/AccountProfileViewModel.cs
+++ src/CollectIt.MVC/CollectIt.MVC.View/ViewModels/AccountProfileViewModel.cs
@@ -5,9 +5,11 @@ namespace CollectIt.MVC.View.ViewModels;
 public class ProfileAccountViewModel
 {
     [Required]
+    [MinLength(6)]
     public string Username { get; set; }
-    
+
     [Required]
     [DataType(DataType.EmailAddress)]
+    [MinLength(8)]
     public string Email { get; set; }
 }
