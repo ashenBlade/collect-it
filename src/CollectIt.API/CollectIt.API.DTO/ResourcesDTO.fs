9b551956056a0b2927aea99b32ed8bf5b0fe9f42
ash.blade <htcdnsnnkom2002@gmail.com>
2022-05-29 07:44:18 +0300

    fix: add Id to ReadDTO for resources

--- src/CollectIt.API/CollectIt.API.DTO/ResourcesDTO.fs
+++ src/CollectIt.API/CollectIt.API.DTO/ResourcesDTO.fs
@@ -2,172 +2,170 @@
 
 open System
 open System.ComponentModel.DataAnnotations
-open System.IO
-open CollectIt.Database.Entities.Resources
 open Microsoft.AspNetCore.Http
 
 [<CLIMutable>]
-type ReadImageDTO = {
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime 
-}
+type ReadImageDTO =
+    { [<Required>]
+      Id: int
+
+      [<Required>]
+      OwnerId: int
+
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime }
 
 
 [<CLIMutable>]
-type ReadMusicDTO = {
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime
-    
-    [<Required>]
-    Duration: int
-}
+type ReadMusicDTO =
+    { [<Required>]
+      Id: int
 
-let ReadImageDTO ownerId name tags extension uploadDate = {
-    OwnerId = ownerId
-    Name = name
-    Tags = tags
-    Extension = extension
-    UploadDate = uploadDate
-}
+      [<Required>]
+      OwnerId: int
 
-let ReadMusicDTO ownerId name tags extension uploadDate duration = {
-    OwnerId = ownerId
-    Name = name
-    Tags = tags
-    Extension = extension
-    UploadDate = uploadDate
-    Duration = duration
-}
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime
+
+      [<Required>]
+      Duration: int }
+
+let ReadImageDTO id ownerId name tags extension uploadDate =
+    { Id = id
+      OwnerId = ownerId
+      Name = name
+      Tags = tags
+      Extension = extension
+      UploadDate = uploadDate }
+
+let ReadMusicDTO id ownerId name tags extension uploadDate duration =
+    { Id = id
+      OwnerId = ownerId
+      Name = name
+      Tags = tags
+      Extension = extension
+      UploadDate = uploadDate
+      Duration = duration }
 
 
-
-[<CLIMutable>]
-type CreateImageDTO = {
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime
-    
-    [<Required>]
-    Content: IFormFile
-}
 
 [<CLIMutable>]
-type CreateMusicDTO = {
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime
-    
-    [<Required>]
-    Content: IFormFile
-    
-    [<Required>]
-    [<Range(1, Int32.MaxValue)>]
-    Duration: int
-}
+type CreateImageDTO =
+    { [<Required>]
+      OwnerId: int
+
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime
+
+      [<Required>]
+      Content: IFormFile }
 
 [<CLIMutable>]
-type ReadVideoDTO = {
-    [<Required>]
-    Id: int
-    
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime
-    
-    [<Required>]
-    Duration: int
-}
+type CreateMusicDTO =
+    { [<Required>]
+      OwnerId: int
 
-let ReadVideoDTO id ownerId extension name tags duration uploadDate: ReadVideoDTO =
-    let x: ReadVideoDTO = {
-        Id = id
-        OwnerId = ownerId
-        Extension = extension
-        Name = name
-        Tags = tags
-        Duration = duration
-        UploadDate = uploadDate
-    }
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime
+
+      [<Required>]
+      Content: IFormFile
+
+      [<Required>]
+      [<Range(1, Int32.MaxValue)>]
+      Duration: int }
+
+[<CLIMutable>]
+type ReadVideoDTO =
+    { [<Required>]
+      Id: int
+
+      [<Required>]
+      OwnerId: int
+
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime
+
+      [<Required>]
+      Duration: int }
+
+let ReadVideoDTO id ownerId extension name tags duration uploadDate : ReadVideoDTO =
+    let x: ReadVideoDTO =
+        { Id = id
+          OwnerId = ownerId
+          Extension = extension
+          Name = name
+          Tags = tags
+          Duration = duration
+          UploadDate = uploadDate }
+
     x
-    
+
 [<CLIMutable>]
-type CreateVideoDTO = {
-    [<Required>]
-    OwnerId : int
-    
-    [<Required>]
-    Name : string
-    
-    [<Required>]
-    Tags : string[]
-    
-    [<Required>]
-    Extension : string
-    
-    [<Required>]
-    UploadDate : DateTime
-    
-    [<Required>]
-    [<Range(1, Int32.MaxValue)>]
-    Duration: int
-    
-    [<Required>]
-    Content: IFormFile
-}
+type CreateVideoDTO =
+    { [<Required>]
+      OwnerId: int
+
+      [<Required>]
+      Name: string
+
+      [<Required>]
+      Tags: string []
+
+      [<Required>]
+      Extension: string
+
+      [<Required>]
+      UploadDate: DateTime
+
+      [<Required>]
+      [<Range(1, Int32.MaxValue)>]
+      Duration: int
+
+      [<Required>]
+      Content: IFormFile }
